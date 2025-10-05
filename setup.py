a35a0111e728653ea391c4e385e9013a5687e429
Federico Moretti <hello@federicomoretti.it>
2025-10-05 18:24:44 +0200

    chore: add install and setup scripts

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+setup(
+    name = 'text_to_baby_talk_fedtti',
+    version = '0.0.1',
+    packages = ['text_to_baby_talk_fedtti'],
+    entry_points = {
+        'console_scripts': [
+            'text_to_baby_talk_fedtti = text_to_baby_talk_fedtti.__main__:main'
+        ]
+    }
+)
